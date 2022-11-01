795c25602cb84364f1c2e242eff62ab187cc1dd0
Dae-une <ko301201@naver.com>
2022-11-01 18:08:35 +0900

    Add: Benefit

--- components/campaign/Benefit/Benefit.js
+++ components/campaign/Benefit/Benefit.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
+import { benefits } from '../../../Static/Benefit/Benefit';
 import * as styles from './styles';
 import FadeIn from '../../common/FadeIn/FadeIn';
 
 const Benefit = () => {
-  return;
+  return (
+    <section css={styles.BenefitSection}>
+      <div css={styles.TitleWrapper}>
+        <FadeIn delay={0}>
+          <h2 css={styles.Title}>이번 캠페인에서만 주어지는 혜택이에요</h2>
+        </FadeIn>
+        <FadeIn delay={50}>
+          <p css={styles.Pre}>군더더기를 모두 빼고 채용의 본질에만 집중했어요.</p>
+        </FadeIn>
+      </div>
+      <div css={styles.ContentWrapper}>
+        <ul css={styles.List}>
+          {benefits.map((benefit, idx) => (
+            <FadeIn key={benefit.title} delay={(idx + 2) * 50}>
+              <li css={styles.BenefitList}>
+                <h3 css={styles.BenefitTitle}>{benefit.title}</h3>
+                <p css={styles.BenefitSub}>{benefit.subTitle}</p>
+              </li>
+            </FadeIn>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Benefit;
--- components/campaign/Benefit/styles.js
+++ components/campaign/Benefit/styles.js
@@ -1,1 +1,44 @@
 import { css } from '@emotion/react';
+
+export const BenefitSection = css`
+  margin-inline: auto;
+  margin-top: 8rem;
+  max-width: 60rem;
+`;
+
+export const TitleWrapper = css``;
+export const Title = css`
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 135%;
+`;
+export const Pre = css`
+  margin-top: 0.75rem;
+  font-size: 1.125rem;
+  line-height: 162%;
+`;
+export const ContentWrapper = css`
+  margin-top: 3rem;
+`;
+export const List = css`
+  gap: 2.25rem;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+`;
+export const BenefitList = css`
+  gap: 0.75rem;
+  display: flex;
+  flex-direction: column;
+`;
+export const BenefitTitle = css`
+  font-size: 2rem;
+  font-weight: 700;
+  line-height: 135%;
+`;
+export const BenefitSub = css`
+  font-size: 1.25rem;
+  color: #adb1ba;
+`;
